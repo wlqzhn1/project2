c640a1ed0274be9635bcd27f0c9e71261394fbef
wlq <wlqzhn@126.com>
2016-12-08 12:53:41 +0800

    update 04

--- src/com/feifei/hello/HelloWorld.java
+++ src/com/feifei/hello/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("我的第一个EGit项目");
 		System.out.println("update 01");
+		System.out.println("我的第一次修改...");		
 		System.out.println("update 02");
+		System.out.println("update 03");
 	}
 }
