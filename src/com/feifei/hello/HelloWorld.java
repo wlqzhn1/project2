5b71fe34a8ccb7e11c4977e1e2c66cb5ee0c8518
shyboy_fei <shyboy_fei@sina.com>
2016-12-08 13:04:29 +0800

    aaa

--- src/com/feifei/hello/HelloWorld.java
+++ src/com/feifei/hello/HelloWorld.java
@@ -7,9 +7,6 @@ public class HelloWorld {
 		System.out.println("我的第一次修改...");		
 		System.out.println("update 02");
 		System.out.println("update 03");
-<<<<<<< HEAD
 		System.out.println("update 04");
-=======
->>>>>>> refs/remotes/origin/master
 	}
 }
